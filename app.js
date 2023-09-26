40faa790aa0439e1f7f6fd02763eb7403cc5d945
SUEN4G4 <suenaga.sy@gmail.com>
2023-09-26 15:09:12 +0900

    clone

--- app.js
+++ app.js
@@ -10,22 +10,11 @@ const { time } = require("./main/time.js");
 const { getCalendars } = require("./main/calendar.js");
 
 const config = {
-  testv2: {
-    channelAccessToken: process.env.TESTV2_ACCESS_TOKEN,
-    channelSecret: process.env.TESTV2_SECRET,
-  },
-  test: {
-    channelAccessToken: process.env.TEST_ACCESS_TOKEN,
-    channelSecret: process.env.TEST_SECRET,
-  },
 };
 
-const channelSecretArray = [process.env.TESTV2_SECRET, process.env.TEST_SECRET];
+const channelSecretArray = [];
 
-const botIdArray = {
-  testv2: process.env.TESTV2_BOTID,
-  test: process.env.TEST_BOTID,
-};
+const botIdArray = {};
 
 let lineClient;
 let name;
@@ -40,15 +29,14 @@ exports.webhook = onRequest(
 
     if (validate_signature(req, channelSecretArray)) {
       switch (req.body.destination) {
-        //testv2
-        case botIdArray.testv2:
-          lineClient = new line.Client(config.testv2);
-          name = "testv2";
+
+        case botIdArray:
+          lineClient = new line.Client(config.);
+          name = "";
           break;
-        //test
-        case botIdArray.test:
-          lineClient = new line.Client(config.test);
-          name = "test";
+        case botIdArray:
+          lineClient = new line.Client(config.);
+          name = "";
           break;
 
         default:
@@ -75,13 +63,7 @@ exports.webhook = onRequest(
             const username = await getNameById(id, name);
             const docid = await addNewBooking(id, username, name);
             lineClient.replyMessage(events.replyToken, JSON.parse(await course(name, docid)));
-            getCalendars();
-            /*if (username === "新規") {
-            main.profileEntry(id, name);
-          } else {
-            lineClient.replyMessage(events.replyToken, course.course(name)); //コース選択
-            main.addNewBooking(id, username, name);
-          } */
+            
           }
           //コース選択完了
           else if (postback.status == "course") {
@@ -93,18 +75,7 @@ exports.webhook = onRequest(
             getLatestBooking(name, docid, "calendar", array[3]);
             getLatestBooking(name, docid, "menu", array[4]);
             lineClient.replyMessage(events.replyToken, JSON.parse(await date(name, docid)));
-            /* const flg = main.getLatestBooking(id, 4, array[0], name);
-            if (flg == true) {
-              //main.date(replytoken, name); //日付選択へ
-              main.getLatestBooking(id, 5, array[1], name);
-              main.getLatestBooking(id, 6, array[2], name);
-              main.freeDate(replytoken, id, name);
-              main.getLatestBooking(id, 10, array[3], name);
-            } else if (flg == false) {
-              main.error(replytoken, name);
-            } else {
-              main.bookingError(replytoken, name);
-            } */
+
           }
           //日付選択完了
           else if (postback.status == "date") {
@@ -113,14 +84,7 @@ exports.webhook = onRequest(
             const docid = array[0];
             getLatestBooking(name, docid, "date", new Date(array[1]));
             lineClient.replyMessage(events.replyToken, JSON.parse(await time(name, docid)));
-            /*   const flg = main.getLatestBooking(id, 3, date, name);
-            if (flg == true) {
-              main.time(replytoken, id, name); //時間選択
-            } else if (flg == false) {
-              main.error(replytoken, name);
-            } else {
-              main.bookingError(replytoken, name);
-            } */
+
           }
           //時間選択完了
           else if (postback.status == "time") {
@@ -136,73 +100,7 @@ exports.webhook = onRequest(
               main.bookingError(replytoken, name);
             }
           }
-          //option選択完了
-          else if (postback.status == "option") {
-            const option = postback.value;
-            const array = option.split("-");
-            const flg = main.getLatestBooking(id, 11, array[0], name);
-            if (flg == true) {
-              main.conf(replytoken, id, name);
-              main.getLatestBooking(id, 12, array[1], name);
-            } else if (flg == false) {
-              main.error(replytoken, name);
-            } else {
-              main.bookingError(replytoken, name);
-            }
-          }
-          //時間帯選択完了　ベーシックプラン
-          else if (postback.status == "conf") {
-            if (id == main.masterAccount(name)) {
-              main.masterUserList(replytoken, name); //マスター
-            } else {
-              const flg = main.getLatestBooking(id, 8, "確定", name);
-              if (flg == true) {
-                main.fin(replytoken, id, name);
-              } else if (flg == false) {
-                main.error(replytoken, name);
-              } else {
-                main.bookingError(replytoken, name);
-              }
-            }
-          } else if (postback.status == "userList") {
-            /* ======= master（ベーシックプラン） ======= */
-            const userId = postback.value;
-            main.masterConf(replytoken, id, name, userId);
-          } else if (postback.status == "masterConf") {
-            const userId = postback.value;
-            main.masterFin(replytoken, id, name, userId);
-            const flg = main.getLatestBooking(userId, 8, "確定", name);
-            if (flg == false) {
-              main.error(replytoken, name);
-            }
-          }
-        } else if (postback.action == "other") {
-          /* =============メニュー============= */
-          if (postback.status == "start") {
-            const username = main.getNameById(id, name);
-            if (username === "新規") {
-              main.profileEntry(id, name);
-            } else {
-              main.menu(replytoken, name);
-            }
-          }
-          //予約履歴開始
-          else if (postback.status == "history") {
-            main.history(replytoken, id, name);
-          } else if (postback.status == "cancel") {
-            const rowNum = postback.value;
-            main.cancel(replytoken, rowNum, name);
-          } else if (postback.status == "conf") {
-            const rowNum = postback.value;
-            main.cancelfin(replytoken, rowNum, name);
-            main.addRowBooking(rowNum, 8, "キャンセル", name);
-          } else if (postback.status == "profile") {
-            main.profile(replytoken, id, name);
-          } else if (postback.status == "tariff") {
-            main.tariff(replytoken, name);
-          }
-        }
-      }
+
       //messageイベント
       else if (events.type == "message") {
         //users.getNameById(id, name);
--- main/courses.js
+++ main/courses.js
@@ -3,19 +3,6 @@ const { color } = require("./others");
 
 const db = database();
 
-/* const menuData = async (name) => {
-  let array = new Array();
-  const menuCollection = (await db.collection("clients").doc(name).collection("menu").get()).docs;
-  for (let [i, doc] of menuCollection.entries()) {
-    array.push(doc.data());
-    array[i].button = [];
-    const buttonCollection = (await doc.ref.collection("button").get()).docs;
-    for (let doc of buttonCollection) {
-      array[i].button.push(doc.data());
-    }
-  }
-  return array;
-}; */
 const menuData = async (name) => {
   const menuRef = db.collection("clients").doc(name).collection("menu");
   const menuSnapshot = await menuRef.get();
@@ -127,80 +114,3 @@ const courseButton = (array, docid, color) => {
   }
   return JSON.stringify(payload);
 };
-
-/*
-//ロゴ
-function course_logo(replytoken, name) {
-  const info = initLine(name);
-  const payload = {
-    replyToken: replytoken,
-    messages: [
-      {
-        type: "flex",
-        altText: "this is a flex message",
-        contents: {
-          type: "carousel",
-          contents: [],
-        },
-      },
-    ],
-  };
-  const array = info.sheet.getRange(9, 1, 13, 13).getValues();
-  for (let i = 1; i <= array[0][0]; i++) {
-    let msg = JSON.parse(coursejson_logo(i, array, name));
-    payload.messages[0].contents.contents.push(msg);
-  }
-
-  const options = {
-    method: "post",
-    headers: info.json_header,
-    payload: JSON.stringify(payload),
-  };
-  return UrlFetchApp.fetch(info.url.reply, options);
-}
-
-//コース
-function coursejson_logo(col, array, name) {
-  const payload = {
-    type: "bubble",
-    header: {
-      type: "box",
-      layout: "vertical",
-      contents: [
-        {
-          type: "image",
-          url: "https://apps.smartbooker.net/img/aruba_logo.jpg",
-          size: "md",
-        },
-      ],
-    },
-    hero: {
-      type: "box",
-      layout: "vertical",
-      contents: [
-        {
-          type: "text",
-          text: "【 " + array[1][col] + " 】",
-          size: "xl",
-          align: "center",
-          weight: "bold",
-        },
-        {
-          type: "text",
-          text: "ご希望のコースを選択してください。",
-          margin: "sm",
-          align: "center",
-        },
-      ],
-    },
-    body: {
-      type: "box",
-      layout: "vertical",
-      contents: JSON.parse(course_button(col, array, name)),
-      spacing: "xs",
-      margin: "xs",
-    },
-  };
-  return JSON.stringify(payload);
-}
- */
